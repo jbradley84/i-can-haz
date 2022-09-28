e4fa8276fffab414934330578d5c25cffe6e236c
jbradley84 <utbradley84@yahoo.com>
2022-09-27 20:07:57 -0500

    created initial model structures

---
+++ server/models/Collection.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+
+const collectionSchema = new Schema(
+  {
+    collectionName: {
+      type: String
+    },
+    collectionDescription: {
+      type: String
+    },
+    username: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      //required: true
+    },
+    items: [
+      {
+         type: Schema.Types.ObjectId,
+         ref: 'Item'
+      }
+    ],
+    comments: [
+      {
+         type: Schema.Types.ObjectId,
+         ref: 'Comment'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+collectionSchema.virtual('itemCount').get(function() {
+  return this.items.length;
+});
+
+collectionSchema.virtual('commentCount').get(function() {
+  return this.comments.length;
+});
+
+const Collection = model('Collection', collectionSchema);
+
+module.exports = Collection;
---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const Collection = require('./Collection');
+const Comment = require('./Comment');
+const Item = require('./Item');
+
+module.exports = { User, Collection, Comment, Item };
