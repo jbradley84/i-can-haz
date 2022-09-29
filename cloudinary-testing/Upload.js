6ceaddaa707e32e73a3701bd05a2e7c363af58d9
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-09-28 20:54:07 -0500

    add minor upload changes

--- cloudinary-testing/Upload.js
+++ cloudinary-testing/Upload.js
@@ -10,7 +10,7 @@ export default function Upload() {
         const image = e.target.images[0];
         imagePreview(image);
 
-    }
+    };
 
     const imagePreview = (image) => {
         const reader = new FileReader();
@@ -18,11 +18,23 @@ export default function Upload() {
         reader.onloadend = () => {
             setPreviewSrc(reader.result);
         }
+    };
+
+    const handleImageSubmit = (e) => {
+        e.preventDefault();
+        if(!preveiwSrc) return;
+        uploadImage(preveiwSrc);
+
+    };
+
+    const uploadImage = () => {
+
     }
+
     return(
         <div>
             <h2>Upload Image</h2>
-            <form>
+            <form onSubmit={handleImageSubmit}>
                 <input type="file" name="image" onChange={handleImageInputChange} value ={imageInputState} className="upload-form-input"></input>
                 <button className="btn" type="submit">Submit</button>
             </form>
