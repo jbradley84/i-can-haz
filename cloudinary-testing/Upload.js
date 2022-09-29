73a86791b16f8938caefbaafb6571b9da5fa287e
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-09-28 19:42:38 -0500

    add current image state

--- cloudinary-testing/Upload.js
+++ cloudinary-testing/Upload.js
@@ -1,11 +1,17 @@
 import React, {useState} from "react";
 
 export default function Upload() {
+
+    const [imageInputState, setimageInputState] = useState('');
+    const handleImageInputChange = (e) => {
+        const image = e.target.images[0];
+    }
+
     return(
         <div>
-            <h2>Image Upload</h2>
+            <h2>Upload Image</h2>
             <form>
-                <input type="file" name="image" className="form-input"></input>
+                <input type="file" name="image" onChange={handleImageInputChange} value ={imageInputState} className="form-input"></input>
                 <button className="btn" type="submit">Submit</button>
             </form>
         </div>
