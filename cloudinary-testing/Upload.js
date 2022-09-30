80fc1e6572481fa4f639bf3112664cc39aa36f37
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-09-29 21:08:38 -0500

    add image preview and form wrapping

--- cloudinary-testing/Upload.js
+++ cloudinary-testing/Upload.js
@@ -1,46 +1,46 @@
-import React, {useState} from "react";
+// import React, {useState} from "react";
 
-export default function Upload() {
+// export default function Upload() {
 
-    const [imageInputState, setimageInputState] = useState('');
-    // const [selectedImage, setSelectedImage] = useState('');
-    const [preveiwSrc, setPreviewSrc] = useState('');
+//     const [imageInputState, setimageInputState] = useState('');
+//     const [selectedImage, setSelectedImage] = useState('');
+//     const [preveiwSrc, setPreviewSrc] = useState('');
 
-    const handleImageInputChange = (e) => {
-        const image = e.target.images[0];
-        imagePreview(image);
+//     const handleImageInputChange = (e) => {
+//         const image = e.target.files[0];
+//         imagePreview(image);
 
-    };
+//     };
 
-    const imagePreview = (image) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(image);
-        reader.onloadend = () => {
-            setPreviewSrc(reader.result);
-        }
-    };
+//     const imagePreview = (image) => {
+//         const reader = new FileReader();
+//         reader.readAsDataURL(image);
+//         reader.onloadend = () => {
+//             setPreviewSrc(reader.result);
+//         }
+//     };
 
-    const handleImageSubmit = (e) => {
-        e.preventDefault();
-        if(!preveiwSrc) return;
-        uploadImage(preveiwSrc);
+//     const handleImageSubmit = (e) => {
+//         e.preventDefault();
+//         if(!preveiwSrc) return;
+//         uploadImage(preveiwSrc);
 
-    };
+//     };
 
-    const uploadImage = () => {
+//     const uploadImage = () => {
 
-    }
+//     }
 
-    return(
-        <div>
-            <h2>Upload Image</h2>
-            <form onSubmit={handleImageSubmit}>
-                <input type="file" name="image" onChange={handleImageInputChange} value ={imageInputState} className="upload-form-input"></input>
-                <button className="btn" type="submit">Submit</button>
-            </form>
-            {preveiwSrc && (
-                <img src={preveiwSrc} alt="image preview" ></img>
-            )}
-        </div>
-    )
-}
+//     return(
+//         <div>
+//             <h2>Upload Image</h2>
+//             <form onSubmit={handleImageSubmit}>
+//                 <input type="file" name="image" onChange={handleImageInputChange} value ={imageInputState} className="upload-form-input"></input>
+//                 <button className="btn" type="submit">Submit</button>
+//             </form>
+//             {preveiwSrc && (
+//                 <img src={preveiwSrc} alt="image preview" ></img>
+//             )}
+//         </div>
+//     )
+// }
--- cloudinary-testing/Uploadv2.js
+++ cloudinary-testing/Uploadv2.js
@@ -1,21 +1,49 @@
-// import React, {useState} from "react";
-// import Axios from "axios";
+import React, {useState} from "react";
+import Image from "cloudinary-react";
+import Axios from "axios";
 
-// const App = () => {
-//     const [selectedImage, setSelectedImage] = useState('');
+const App = () => {
+    const [selectedImage, setSelectedImage] = useState('');
+    const [preveiwSrc, setPreviewSrc] = useState('');
 
-//     const uploadImage = () => {
-//         const formData = new FormData();
-//         formData.append("file", selectedImage);
-//         formData.append("upload_preset", "collectaur");
+    const uploadImage = () => {
+        const formData = new FormData();
+        formData.append("file", selectedImage);
+        formData.append("upload_preset", "collectaur");
 
-//         Axios.post("https://api.cloudinary.com/v1_1/dgbv72kqf/image/upload", formData);
-//     };
-//     return(
-//         <div>
-//             <input type="file" ></input>
-//         </div>
-//     )
-// };
+        Axios.post("https://api.cloudinary.com/v1_1/dgbv72kqf/image/upload", formData)
+        .then((response) => {
+            console.log(response);
+        })
+    };
 
-// export default App;
+    const imagePreview = (image) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(image);
+        reader.onloadend = () => {
+            setPreviewSrc(reader.result);
+        }
+    };
+
+    const handleImageInputChange = (e) => {
+        const imageFile = e.target.files[0];
+        setSelectedImage(imageFile);
+        imagePreview(imageFile);
+
+    };
+
+
+    return(
+        <div>
+            <form>
+                <input type="file" onChange={handleImageInputChange}></input>
+                <button onClick={uploadImage}></button>
+            </form>
+            {preveiwSrc && (
+                <img src={preveiwSrc} alt="preview" ></img>
+                )}
+        </div>
+    )
+};
+
+export default App;
