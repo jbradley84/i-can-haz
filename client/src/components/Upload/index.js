9d1659a452c7bc37c656b5734424d97d596dda11
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-10-01 16:34:39 -0500

    add base widget

--- client/src/components/Upload/index.js
+++ client/src/components/Upload/index.js
@@ -1,15 +1,34 @@
+import React, { Component } from "react";
 
+class Upload extends Component {
+  componentDidMount() {
+    const myWidget = window.cloudinary.createUploadWidget(
+      {
+        cloudName: "dgbv72kqf",
+        uploadPreset: "collectaur",
+      },
+      (error, result) => {
+        if (!error && result && result.event === "success") {
+          console.log("Done! Here is the image info: ", result.info);
+        }
+      }
+    );
+    document.getElementById("upload_widget").addEventListener(
+      "click",
+      function () {
+        myWidget.open();
+      },
+      false
+    );
+  }
 
-import React from "react";
+  render() {
+    return (
+      <button id="upload_widget" className="cloudinary-button">
+        Upload Image
+      </button>
+    );
+  }
+}
 
-export default function Upload() {
-
-    return(
-        <div>
-            <form>
-                <input type="file"></input>
-                <button>Upload Image</button>
-            </form>
-        </div>
-    )
-};
+export default Upload;
