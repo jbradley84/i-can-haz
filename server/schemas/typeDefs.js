3562678e237051d6cae3fa9660f54640dc8d3b15
jbradley84 <utbradley84@yahoo.com>
2022-10-01 12:26:06 -0500

    merging develop into feature branch

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -45,8 +45,8 @@ const typeDefs = gql`
    }
 
    type Mutation {
-      login(email: String!, password: String!): Auth
-      addUser(username: String!, email: String!, password: String!): Auth
+      login(email: String!, password: String!): User
+      addUser(username: String!, email: String!, password: String!): User
       addCollection(collectionName: String!, collectionDescription: String!): Collection
       addItem(collectionId: ID!, itemName: String!, itemImage: String!, itemDescription: String!): Collection
       addComment(collectionId: ID!, commentBody: String!): Collection
