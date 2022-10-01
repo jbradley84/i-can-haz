1c3adcdca0330c9c00d1bb6d872844057cc8de55
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-10-01 14:18:10 -0500

    revery component to form skeleton for widget

---
+++ client/src/components/Upload/index.js
@@ -0,0 +1,15 @@
+
+
+import React from "react";
+
+export default function Upload() {
+
+    return(
+        <div>
+            <form>
+                <input type="file"></input>
+                <button>Upload Image</button>
+            </form>
+        </div>
+    )
+};
