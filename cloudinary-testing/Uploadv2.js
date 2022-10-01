53e94bf803f3eba975e8c13eabb3974f14a2b786
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-09-30 20:37:17 -0500

    add upload functionality, needs response log fix

--- cloudinary-testing/Uploadv2.js
+++ cloudinary-testing/Uploadv2.js
@@ -1,6 +1,9 @@
+
+
 import React, {useState} from "react";
-import Image from "cloudinary-react";
+import {Image} from "cloudinary-react";
 import Axios from "axios";
+import { Cloudinary } from "@cloudinary/url-gen";
 
 const App = () => {
     const [selectedImage, setSelectedImage] = useState('');
@@ -10,39 +13,42 @@ const App = () => {
         const formData = new FormData();
         formData.append("file", selectedImage);
         formData.append("upload_preset", "collectaur");
+        console.log(formData);
 
         Axios.post("https://api.cloudinary.com/v1_1/dgbv72kqf/image/upload", formData)
         .then((response) => {
             console.log(response);
-        })
+        });
     };
 
-    const imagePreview = (image) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(image);
-        reader.onloadend = () => {
-            setPreviewSrc(reader.result);
-        }
-    };
+    // const imagePreview = (selectedImage) => {
+    //     const reader = new FileReader();
+    //     reader.readAsDataURL(selectedImage);
+    //     reader.onloadend = () => {
+    //         setPreviewSrc(reader.result);
+    //     }
+    // };
 
-    const handleImageInputChange = (e) => {
-        const imageFile = e.target.files[0];
-        setSelectedImage(imageFile);
-        imagePreview(imageFile);
+    // const handleImageInputChange = (e) => {
+    //     const imageFile = e.target.files[0];
+    //     setSelectedImage(imageFile);
+    //     imagePreview(imageFile);
+    //     console.log(imageFile);
+    //     console.log(selectedImage);
 
-    };
+    // };
 
 
     return(
         <div>
             <form>
-                <input type="file" onChange={handleImageInputChange}></input>
+                <input type="file" onChange={(e) => {setSelectedImage(e.target.files[0])}}></input>
                 <button onClick={uploadImage}>Upload Image</button>
             </form>
             {preveiwSrc && (
                 <img src={preveiwSrc} alt="preview" ></img>
                 )}
-        <Image cloudName="dgbv72kqf" public_id="https://res.cloudinary.com/dgbv72kqf/image/upload/v1664328544/cld-sample-5.jpg"></Image>
+        <Image cloudName="dgbv72kqf" public_id="https://res.cloudinary.com/dgbv72kqf/image/upload/v1664328544/cld-sample-5.jpg"/>
         </div>
     )
 };
