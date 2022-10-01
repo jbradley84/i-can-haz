da6afbddb88110390b6718c5f99e7fb9cfda19f2
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-09-30 23:00:51 -0500

    add cloudinary upload v3, needs full rewrite

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Upload from './cloudinary-testing/Uploadv3';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Upload></Upload>
     </div>
   );
 }
