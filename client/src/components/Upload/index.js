9b6843db438e87e8ba4a1771e4430f6c7660a9f4
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-10-02 13:46:42 -0500

    convert widget from class to module

--- client/src/components/Upload/index.js
+++ client/src/components/Upload/index.js
@@ -1,34 +1,67 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-class Upload extends Component {
-  componentDidMount() {
-    const myWidget = window.cloudinary.createUploadWidget(
-      {
-        cloudName: "dgbv72kqf",
-        uploadPreset: "collectaur",
-      },
-      (error, result) => {
-        if (!error && result && result.event === "success") {
-          console.log("Done! Here is the image info: ", result.info.url);
-        }
+// class Upload extends Component {
+//   componentDidMount() {
+//     const myWidget = window.cloudinary.createUploadWidget(
+//       {
+//         cloudName: "dgbv72kqf",
+//         uploadPreset: "collectaur",
+//       },
+//       (error, result) => {
+//         if (!error && result && result.event === "success") {
+//           console.log("Done! Here is the image info: ", result.info.url);
+//         }
+//       }
+//     );
+//     document.getElementById("upload_widget").addEventListener(
+//       "click",
+//       function () {
+//         myWidget.open();
+//       },
+//       false
+//     );
+//   }
+
+//   render() {
+//     return (
+//       <button id="upload_widget" className="cloudinary-button">
+//         Upload Image
+//       </button>
+//     );
+//   }
+// }
+
+// export default Upload;
+
+import React, { useState } from "react";
+
+export default function Upload() {
+  const [imageUrl, setImageUrl] = useState("");
+
+  const imageWidget = window.cloudinary.openUploadWidget(
+    {
+      cloudName: "dgbv72kqf",
+      uploadPreset: "collectaur",
+    },
+    (error, result) => {
+      if (!error && result && result.event === "success") {
+        console.log("Done! Here is the image info: ", result.info.url);
+        setImageUrl(result.info.url);
       }
-    );
-    document.getElementById("upload_widget").addEventListener(
-      "click",
-      function () {
-        myWidget.open();
-      },
-      false
-    );
-  }
+    }
+  );
+  imageWidget.open();
 
-  render() {
-    return (
-      <button id="upload_widget" className="cloudinary-button">
+  return (
+    <div>
+      <img src={imageUrl} alt="upload preview"></img>
+      <button
+        id="upload_widget"
+        className="cloudinary-button"
+        onClick={imageWidget}
+      >
         Upload Image
       </button>
-    );
-  }
+    </div>
+  );
 }
-
-export default Upload;
