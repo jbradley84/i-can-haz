819aac638a9bb0731ec1db12f4b7b67e00429072
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-10-02 14:18:54 -0500

    add comments to upload

--- client/src/components/Upload/index.js
+++ client/src/components/Upload/index.js
@@ -1,52 +1,23 @@
-// import React, { Component } from "react";
-
-// class Upload extends Component {
-//   componentDidMount() {
-//     const myWidget = window.cloudinary.createUploadWidget(
-//       {
-//         cloudName: "dgbv72kqf",
-//         uploadPreset: "collectaur",
-//       },
-//       (error, result) => {
-//         if (!error && result && result.event === "success") {
-//           console.log("Done! Here is the image info: ", result.info.url);
-//         }
-//       }
-//     );
-//     document.getElementById("upload_widget").addEventListener(
-//       "click",
-//       function () {
-//         myWidget.open();
-//       },
-//       false
-//     );
-//   }
-
-//   render() {
-//     return (
-//       <button id="upload_widget" className="cloudinary-button">
-//         Upload Image
-//       </button>
-//     );
-//   }
-// }
-
-// export default Upload;
 
 import React, { useState } from "react";
 
 export default function Upload() {
+  //sets a state to capture the cloud URL to display the image later
   const [imageUrl, setImageUrl] = useState("");
 
+  //function called when the upload button is pressed to create and open the upload widget
   const imageWidget = () => {
+    //the widget creation needs to be a seperate object within the function to not open immediately upon page load.
     let widget = window.cloudinary.createUploadWidget(
       {
         cloudName: "dgbv72kqf",
         uploadPreset: "collectaur",
       },
+      //all of the info returned when the image is uploaded is in the result.info object property
       (error, result) => {
         if (!error && result && result.event === "success") {
           console.log("Done! Here is the image info: ", result.info.url);
+          //sets the state image url by targeting the result.info property, but result.info has other useful info that can be captured as well.
           setImageUrl(result.info.url);
         }
       }
