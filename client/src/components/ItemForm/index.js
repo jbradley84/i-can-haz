64435893f5f32ae344083015e54e803488365c4d
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-10-02 22:00:39 -0500

    add item form functionality, state, and styling

--- client/src/components/ItemForm/index.js
+++ client/src/components/ItemForm/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Upload from "../Upload";
 import { useMutation } from "@apollo/client";
-import { ADD_ITEM } from "../utils/mutations";
+//import { ADD_ITEM } from "../utils/mutations";
 
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
@@ -9,13 +9,14 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import FormControl from "@mui/material/FormControl";
 import Container from "@mui/system/Container";
+import Grid from "@mui/material/Grid";
 
 const ItemForm = () => {
   const [imageUrl, setImageUrl] = useState("");
   const [formState, setFormState] = useState({
     title: "",
     description: "",
-    imageUrl: imageUrl,
+    imageUrl: "",
   });
 
   // const [addItem, { error }] = useMutation(ADD_ITEM);
@@ -48,7 +49,8 @@ const ItemForm = () => {
   //sets a state to capture the cloud URL to display the image later
 
   //function called when the upload button is pressed to create and open the upload widget
-  const imageWidget = () => {
+  const imageWidget = (e) => {
+    e.preventDefault();
     //the widget creation needs to be a seperate object within the function to not open immediately upon page load.
     let widget = window.cloudinary.createUploadWidget(
       {
@@ -61,7 +63,7 @@ const ItemForm = () => {
           console.log("Done! Here is the image info: ", result.info.url);
           //sets the state image url by targeting the result.info property, but result.info has other useful info that can be captured as well.
           setImageUrl(result.info.url);
-
+          setFormState({ ...formState, itemUrl: result.info.url });
         }
       }
     );
@@ -70,38 +72,57 @@ const ItemForm = () => {
   //CLOUDINARY UPLOAD END
 
   return (
-    <Container>
-      <Box component="form" noValidate>
-        <TextField
-          required
-          name="title"
-          type="title"
-          id="title"
-          value={formState.title}
-          onChange={handleChange}
-          label="Title"
-          helperText="required*"
-        ></TextField>
+    <Container maxWidth="sm" sx={{ height: "80vh" }}>
+      <Box
+        sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+      >
+        <Box component="form" noValidate sx={{}}>
+          <Grid container spacing={2}>
+            <Grid item>
+              <TextField
+                required
+                name="title"
+                type="title"
+                id="title"
+                value={formState.title}
+                label="Title"
+                helperText="required"
+                onChange={handleChange}
+              ></TextField>
+            </Grid>
 
-        <TextField
-          required
-          name="description"
-          type="description"
-          id="description"
-          value={formState.description}
-          label="Description"
-          helperText="required*"
-        ></TextField>
-        <div>
-          {/* <img src={imageUrl} alt="upload preview"></img> */}
-          <button
-            id="upload_widget"
-            className="cloudinary-button"
-            onClick={imageWidget}
-          >
-            Upload Image
-          </button>
-        </div>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                multiline
+                rows={5}
+                name="description"
+                type="description"
+                id="description"
+                value={formState.description}
+                label="Description"
+                helperText="required"
+                onChange={handleChange}
+              ></TextField>
+            </Grid>
+
+            <Grid item>
+              <Button
+                sx={{
+                    mt: 1,
+                    mb: 2,
+                    ":hover": {
+                        bgcolor: "secondary.main",
+                        color: "white"
+                    }
+                }}
+                variant="contained" onClick={imageWidget}>
+                Upload Image
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
       </Box>
     </Container>
   );
