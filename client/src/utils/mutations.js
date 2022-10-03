8d9bcd10dd5edc7d85e26c086d767f0a71b090de
jbradley84 <utbradley84@yahoo.com>
2022-10-02 20:58:08 -0500

    added mutations/queries to client folder

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -21,7 +21,56 @@ export const ADD_USER = gql`
       user {
         _id
         username
+        email
       }
     }
+  }
+`;
+
+// UPDATE USER MUTATION
+export const UPDATE_USER = gql`
+  mutation updateUser($username: String, $email: String, $password: String) {
+   updateUser(username: $username, email: $email, password: $password) {
+      _id
+      username
+      email
+   }
+  }
+`;
+
+// ADD COLLECTION MUTATION
+export const ADD_COLLECTION = gql`
+  mutation addCollection($collectionName: String!, $collectionDescription: String!) {
+   addCollection(collectionName: $collectionName, collectionDescription: $collectionDescription) {
+      _id
+      collectionName
+      collectionDescription
+      username
+   }
+  }
+`;
+
+// DELETE COLLECTION MUTATION
+export const DELETE_COLLECTION = gql`
+  mutation deleteCollection($collectionId: ID!) {
+   deleteCollection(collectionId: $collectionId) {
+      _id
+   }
+  }
+`;
+
+// ADD ITEM MUTATION
+export const ADD_ITEM = gql`
+  mutation addItem($collectionId: ID!, $itemName: String!, $itemImage: String!, $itemDescription: String!) {
+   addItem(collectionId: $collectionId, itemName: $itemName, itemImage: $itemImage, itemDescription: $itemDescription) {
+      _id
+      itemCount
+      items {
+         _id
+         itemName
+         itemImage
+         itemDescription
+      }
+   }
   }
 `;
