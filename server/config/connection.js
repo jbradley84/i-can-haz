ce159368dc33e2eba9dd0260ce7e4a61173e4451
jbradley84 <utbradley84@yahoo.com>
2022-10-03 22:17:43 -0500

    deploying to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/i-can-haz', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+   useCreateIndex: true,
+   useFindAndModify: false
+})
+console.log(process.env.MONGODB_URI);
 
 module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -8,9 +8,9 @@ const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
+   typeDefs,
+   resolvers,
+   context: authMiddleware
 });
 
 const app = express();
@@ -19,27 +19,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 const startApolloServer = async (typeDefs, resolvers) => {
-  await server.start();
-  // integrate Apollo server with Express application as middleware
-  server.applyMiddleware({ app });
+   await server.start();
+   // integrate Apollo server with Express application as middleware
+   server.applyMiddleware({ app });
 
 
-// Serve up static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+   // Serve up static assets
+   if (process.env.NODE_ENV === 'production') {
+      app.use(express.static(path.join(__dirname, '../client/build')));
+   }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+   app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/build/index.html'));
+   });
 
 
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    })
-  })
+   db.once('open', () => {
+      app.listen(PORT, () => {
+         console.log(`API server running on port ${PORT}!`);
+         console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+      })
+   })
 };
 
 startApolloServer(typeDefs, resolvers);
